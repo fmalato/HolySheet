1a583576315cfdc7904cd75e1415dac36e22ddeb
francescogradi <francescogradi@gmail.com>
2018-12-24 18:46:10 +0100

    Aggiunto un lettore di lunghezza parole sul txt di ground truth, che si occupa di creare un dizionario di utilita'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import binarizer as bin
 import cv2 as cv
+import stringUtils
 
 
 bible = 'Muenchen'
@@ -8,6 +9,13 @@ binar = bin.Binarizer(bible)
 #binar.binarize()
 #binar.rotate('image.png')
 #binar.linesCropping('Gut-027.jpg')
-img = cv.imread('result.png')
-cropped = cv.imread('cropped.png')
-binar.calimero(img, cropped)
+#img = cv.imread('result.png')
+#cropped = cv.imread('cropped.png')
+#binar.calimero(img, cropped)
+
+groundTruth = open("genesis1-20.txt", "r")
+lines = groundTruth.readlines()
+
+dictionary = stringUtils.getWordsCounterDict(lines)
+
+print(dictionary)
---
+++ stringUtils.py
@@ -0,0 +1,35 @@
+# Dato un txt in ingresso, lo legge e crea un dizionario che ha come chiavi il nome della pagina con il numero di
+# colonna (0 o 1), e come valore un array di interi ordinato con il testo che indica la quantita' di parole per ogni
+# riga
+
+def getWordsCounterDict(lines):
+
+    columnsLengthDict = dict()
+
+    for i in range(len(lines)):
+
+        # Trovo nel txt le righe speciali di inizio paragrafo, saranno chiave nel dizionario
+        if lines[i][0] == "_":
+            columnsLengthDict[lines[i][:-2]] = []
+
+            # Per ogni riga adesso conto quante parole ci sono e me lo salvo
+            j = i + 1
+            while j < len(lines) and lines[j][0] is not "_":
+                columnsLengthDict[lines[i][:-2]].append(wordCounter(lines[j]))
+                j += 1
+
+            # per risparmiare cicli adesso riparto dal nuovo paragrafo
+            i = j
+
+    return columnsLengthDict
+
+
+def wordCounter(line):
+
+    words = 0
+
+    for letter in line:
+        if letter is " " or letter is "=":
+            words += 1
+
+    return words
