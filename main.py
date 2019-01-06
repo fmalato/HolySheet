b80335969dbc65876c7d2a902d8cfddaef71d951
francescogradi <francescogradi@gmail.com>
2019-01-06 22:31:22 +0100

    Salvato in json il dizionario del ground truth e nuove soglie

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import binarizer as bin
 import cv2 as cv
 import stringUtils
-
+import json
 
 bible = 'Muenchen'
 binar = bin.Binarizer(bible)
@@ -12,19 +12,19 @@ binar = bin.Binarizer(bible)
 cropped = cv.imread('cropped.png')
 binar.calimero(img, cropped)"""
 
-groundTruth = open("genesis1-20.txt", "r")
-lines = groundTruth.readlines()
-
-dictionary = stringUtils.getWordsCounterDict(lines)
+with open('groundTruthDictionary.json') as groundTruth:
+    dictionary = json.load(groundTruth)
 
 # Problemi patologici su pag14, cattiva segmentazione su pag31
 
-numPage = 18
+
+
+#for numPage in range(14, 33):
+
+numPage = 33
 
 binar.linesCropping('GenesisPages/old/Muenchen/Gut-0{x}.jpg'.format(x=numPage),
                     numPage,
                     '_P{x}_C0'.format(x=(numPage - 14)),
                     '_P{x}_C1'.format(x=(numPage - 14)),
                     dictionary)
-
-
--- stringUtils.py
+++ stringUtils.py
@@ -1,3 +1,5 @@
+import json
+
 # Dato un txt in ingresso, lo legge e crea un dizionario che ha come chiavi il nome della pagina con il numero di
 # colonna (0 o 1), e come valore un array di interi ordinato con il testo che indica la quantita' di parole per ogni
 # riga
@@ -10,12 +12,12 @@ def getWordsCounterDict(lines):
 
         # Trovo nel txt le righe speciali di inizio paragrafo, saranno chiave nel dizionario
         if lines[i][0] == "_":
-            columnsLengthDict[lines[i][:-1]] = []
+            columnsLengthDict[lines[i][:-2]] = []
 
             # Per ogni riga adesso conto quante parole ci sono e me lo salvo
             j = i + 1
             while j < len(lines) and lines[j][0] is not "_":
-                columnsLengthDict[lines[i][:-1]].append(wordCounter(lines[j]))
+                columnsLengthDict[lines[i][:-2]].append(wordCounter(lines[j]))
                 j += 1
 
             # per risparmiare cicli adesso riparto dal nuovo paragrafo
@@ -33,3 +35,13 @@ def wordCounter(line):
             words += 1
 
     return words
+
+# Leggo il ground truth e me lo salvo in un file come dizionario json, per non stare a rileggerlo ad ogni esecuzione
+
+groundTruth = open("genesis1-20.txt", "r")
+lines = groundTruth.readlines()
+
+dictionary = getWordsCounterDict(lines)
+
+with open('groundTruthDictionary.json', 'w') as fp:
+    json.dump(dictionary, fp)
