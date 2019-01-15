9d962f81097709afb01059d5ae48d93edf8f5374
freaky1310 <freaky1310@gmail.com>
2019-01-15 18:19:08 +0100

    Connected components function works, but it must be heavily optimized

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import json
 import pytesseract
 import numpy as np
+import collections
 
 from PIL import Image
 
@@ -84,58 +85,97 @@ def hola(img):
     cv.imshow('img', img)
     cv.waitKey(0)
 
-# TODO: Find a solution to the conflict map problem.
-# There should be 20 components, not 127
+# TODO: Solve intractability of the line
 
 def connectedComponents(image_path):
 
     image = cv.imread(image_path)
+    height, width = image.shape[0], image.shape[1]
 
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    start_row, start_col = int(height*0.45), int(0)
+    # Let's get the ending pixel coordinates (bottom right of cropped top)
+    end_row, end_col = int(height), int(width)
+    croppedImage = image[start_row:end_row , start_col:end_col]
+
+    gray = cv.cvtColor(croppedImage, cv.COLOR_BGR2GRAY)
     _, threshed = cv.threshold(gray, 50, 255, cv.THRESH_BINARY)    # BINARY non INV se gli si passa black n white
 
-    height, width = image.shape[0], image.shape[1]
-    numComponent = 0
+    height, width = croppedImage.shape[0], croppedImage.shape[1]
 
-    components = []
+    numComponent = 1
+
+    components = [[0 for x in range(width)]]
     conflicts = {}
+
     for row in range(height):
         line = []
         for col in range(width):
-            if threshed[row][col] == 255 and (threshed[row][col - 1] == 255 or threshed[row - 1][col] == 255):
-                line.append(numComponent)
-            elif threshed[row][col] == 255 and (threshed[row][col - 1] == 0 and threshed[row - 1][col] == 0):
-                numComponent += 1
-                line.append(numComponent)
-            elif threshed[row][col] == 0:
+            if threshed[row][col] == 0:
                 line.append(0)
-            if threshed[row][col] == 255 and (components[row - 1][col] != line[col - 1]) and \
-                                            components[row - 1][col] != 0 and line[col - 1] != 0:
-                rightOne = line[len(line) - 1]
-                if rightOne not in conflicts.keys():
-                    conflicts[rightOne] = []
-                conflicts[rightOne].append(components[row - 1][col])
-                line.pop(len(line) - 1)
-                line.append(rightOne)
+            elif threshed[row][col] == 255:
+                if threshed[row][col - 1] == 255 or threshed[row - 1][col] == 255:
+                    if col > 0:
+                        if components[row - 1][col] == line[col - 1]:
+                            line.append(line[col - 1])
+                        elif components[row - 1][col] != line[col - 1]:
+                            if components[row - 1][col] != 0 and line[col - 1] != 0:
+                                if line[col - 1] not in conflicts.keys():
+                                    conflicts[line[col - 1]] = []
+                                conflicts[line[col - 1]].append(components[row - 1][col])
+                                line.append(line[col - 1])
+                            else:
+                                if threshed[row][col - 1] == 0:
+                                    line.append(components[row - 1][col])
+                                else:
+                                    line.append(line[col - 1])
+                    else:
+                        if threshed[row - 1][col] == 255:
+                            line.append(components[row - 1][col])
+                        else:
+                            line.append(numComponent)
+
+                elif threshed[row][col - 1] == 0 and threshed[row - 1][col] == 0:
+                    numComponent += 1
+                    line.append(numComponent)
         components.append(line)
 
     for key in conflicts.keys():
         conflicts[key] = list(set(conflicts[key]))
 
-    """for comp in components:
-        for el in comp:
-            if """
+    newKeys = []
+    for key in conflicts.keys():
+        while len(conflicts[key]) > 1:
+            newKeys.append((key, conflicts[key][0]))
+            conflicts[key].pop(0)
+
+    for el in newKeys:
+        if el[1] not in conflicts.keys():
+            conflicts[el[1]] = []
+        conflicts[el[1]].append(el[0])
+        if len(conflicts[el[1]]) > 1:
+            newKeys.append((el[0], conflicts[el[1]][0]))
+            conflicts[el[1]].pop(0)
+
+    conflicts = sortDict(conflicts)
+
+    for key in list(conflicts.keys())[::-1]:
+        for row in range(height):
+            for col in range(width):
+                if key == components[row][col]:
+                    components[row][col] = conflicts[key][0]
+        for k in conflicts.keys():
+            if conflicts[k] == [key]:
+                conflicts[k] = [conflicts[key][0]]
+
+    # with open('comp.json', 'w+') as f:
+        # json.dump(components, f)
 
     image = []
     for comp in components:
         imgLine = []
         for el in comp:
-            if el > 0 and el <= 10:
-                imgLine.append((255, 0, 0))
-            elif el > 10 and el <= 20:
-                imgLine.append((0, 255, 0))
-            elif el > 20:
-                imgLine.append((0, 0, 255))
+            if el != 0:
+                imgLine.append((255, 255, 255))
             else:
                 imgLine.append((0, 0, 0))
         image.append(imgLine)
@@ -146,4 +186,14 @@ def connectedComponents(image_path):
     img.save('generated.png')
 
 
+def sortDict(dictionary):
 
+    keysList = [key for key in dictionary.keys()]
+    keysList.sort()
+
+    newDict = collections.OrderedDict()
+
+    for key in keysList:
+        newDict[key] = dictionary[key]
+
+    return newDict
