32c58e8522e5c894009170c6e6bc7b122d2dba45
freaky1310 <freaky1310@gmail.com>
2019-02-10 18:08:35 +0100

    Working on retrieving analytics.

--- results.py
+++ results.py
@@ -1,7 +1,5 @@
 import json
-import os
 import cv2
-import numpy as np
 
 import utils
 
@@ -15,62 +13,95 @@ with open('JsonUtils/10mostFrequentWords.json', 'r') as fw:
     frequentWordsDict = json.load(fw)
 
 boundingBoxes = {}
+numFound = 0
+numNotFound = 0
+numCompared = 0
+numDetections = 0
+numAnnotations = 0
+
 for img in annotations['images']:
     image = cv2.imread('valid2019/{image_name}'.format(image_name=img['file_name']))
     image_id = int(img['file_name'].strip('.png')) + 188000
     boundingBoxes[image_id] = {}
     boundingBoxes[image_id]['bboxes'] = []
     boundingBoxes[image_id]['annotations'] = []
-    #detections = []
-    #groundTruth = []
+
     for el in bboxes:
         if el['image_id'] == image_id:
             cv2.rectangle(image, (int(el['bbox'][0]), int(el['bbox'][1])),
-                      (int(el['bbox'][0] + el['bbox'][2]), int(el['bbox'][1] + el['bbox'][3])), (0, 255, 0), thickness=1)
-            """cv2.putText(img=image, text=str(frequentWordsDict[el['category_id']]),
+                      (int(el['bbox'][0] + el['bbox'][2]), int(el['bbox'][1] + el['bbox'][3])), (0, 255, 0),
+                       thickness=1)
+            cv2.putText(img=image, text=str(frequentWordsDict[el['category_id']]),
                         org=(int(el['bbox'][0]), int(el['bbox'][1]) - 5), fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                        fontScale=0.4, color=(0, 255, 0))"""
-            #detections.append(el['bbox'])
+                        fontScale=0.4, color=(0, 255, 0))
             boundingBoxes[image_id]['bboxes'].append(el['bbox'])
+            numDetections += 1
 
     for x in annotations['annotations']:
         if x['image_id'] == image_id + 150:
             cv2.rectangle(image, (int(x['bbox'][0]), int(x['bbox'][1])),
                       (int(x['bbox'][0] + x['bbox'][2]), int(x['bbox'][1] + x['bbox'][3])), (0, 0, 255), thickness=1)
-            """cv2.putText(img=image, text=str(frequentWordsDict[x['category_id']]),
+            cv2.putText(img=image, text=str(frequentWordsDict[x['category_id']]),
                         org=(int(x['bbox'][0]), int(x['bbox'][1]) + int(x['bbox'][3] + 5)),
-                        fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(0, 0, 255))"""
-            #groundTruth.append(x['bbox'])
+                        fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(0, 0, 255))
             boundingBoxes[image_id]['annotations'].append(x['bbox'])
+            numAnnotations += 1
 
     with open('bboxes.json', 'w+') as f:
         json.dump(boundingBoxes, f, indent=4)
 
-    # TODO: funziona, ma potrebbe funzionare molto meglio con un sistema per trovare
     for box in boundingBoxes[image_id]['bboxes']:
         notes = []
         for ann in boundingBoxes[image_id]['annotations']:
             if (box[1] >= ann[1] - 20) and (box[1] <= ann[1] + 20):
                 iou = utils.intersectionOverUnion(box, ann)
                 if iou != 0.0 and ann not in notes:
-                    print(ann, image_id)
                     if iou >= 0.5:
                         cv2.putText(img=image, text='Y',
                                     org=(int(box[0]), int(box[1]) + int(box[3] + 5)),
                                     fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(255, 0, 0))
                         notes.append(ann)
+                        numFound += 1
+                        numCompared += 1
                         break
                     else:
                         cv2.putText(img=image, text='N',
                                     org=(int(box[0]), int(box[1]) + int(box[3] + 5)),
                                     fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(255, 0, 0))
                         notes.append(ann)
+                        numNotFound += 1
+                        numCompared += 1
                         break
 
+    #cv2.imwrite('results/comparisons/comp_{n}'.format(n=img['file_name']), image)
 
-    """cv2.imshow('{image}'.format(image=img['file_name']), image)
-    cv2.waitKey(100000)
-    cv2.destroyAllWindows()"""
+percentageFound = round((numFound / numCompared) * 100, 2)
+percentageNotFound = round((numNotFound / numCompared) * 100, 2)
+netCorrections = numDetections - numCompared
+gTruthCorrections = numAnnotations - numCompared
 
-    cv2.imwrite('results/comparisons/comp_{n}'.format(n=img['file_name']), image)
+print('numDetections: {x}'.format(x=numDetections))
+print('numAnnotations: {x}'.format(x=numAnnotations))
+print('numFound: {x}'.format(x=numFound))
+print('numNotFound: {x}'.format(x=numNotFound))
+print('numCompared: {x}'.format(x=numCompared))
+print('percentage of found items: {x}%'.format(x=percentageFound))
+print('percentage of not found items: {x}%'.format(x=percentageNotFound))
+print('words found by the net but not shown in the ground truth: {x}'.format(x=netCorrections))
+print('words shown in the ground truth but not found by the net: {x}'.format(x=gTruthCorrections))
+
+analytics = {}
+analytics['numDetections'] = numDetections
+analytics['numAnnotations'] = numAnnotations
+analytics['numFound'] = numFound
+analytics['numNotFound'] = numNotFound
+analytics['numCompared'] = numCompared
+analytics['percentageFound'] = percentageFound
+analytics['percentageNotFound'] = percentageNotFound
+analytics['netCorrections'] = netCorrections
+analytics['groundTruthCorrections'] = gTruthCorrections
+
+with open('analytics.json', 'w+') as f:
+    json.dump(analytics, f, indent=4)
+
 
