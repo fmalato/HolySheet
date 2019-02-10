7624c00be84d05b7e34b7f7fa55f62785afc9797
freaky1310 <freaky1310@gmail.com>
2019-02-10 16:23:40 +0100

    IoU work as it should.

--- results.py
+++ results.py
@@ -19,8 +19,10 @@ for img in annotations['images']:
     image = cv2.imread('valid2019/{image_name}'.format(image_name=img['file_name']))
     image_id = int(img['file_name'].strip('.png')) + 188000
     boundingBoxes[image_id] = {}
-    detections = []
-    groundTruth = []
+    boundingBoxes[image_id]['bboxes'] = []
+    boundingBoxes[image_id]['annotations'] = []
+    #detections = []
+    #groundTruth = []
     for el in bboxes:
         if el['image_id'] == image_id:
             cv2.rectangle(image, (int(el['bbox'][0]), int(el['bbox'][1])),
@@ -28,7 +30,8 @@ for img in annotations['images']:
             """cv2.putText(img=image, text=str(frequentWordsDict[el['category_id']]),
                         org=(int(el['bbox'][0]), int(el['bbox'][1]) - 5), fontFace=cv2.FONT_HERSHEY_DUPLEX,
                         fontScale=0.4, color=(0, 255, 0))"""
-            detections.append(el['bbox'])
+            #detections.append(el['bbox'])
+            boundingBoxes[image_id]['bboxes'].append(el['bbox'])
 
     for x in annotations['annotations']:
         if x['image_id'] == image_id + 150:
@@ -37,39 +40,33 @@ for img in annotations['images']:
             """cv2.putText(img=image, text=str(frequentWordsDict[x['category_id']]),
                         org=(int(x['bbox'][0]), int(x['bbox'][1]) + int(x['bbox'][3] + 5)),
                         fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(0, 0, 255))"""
-            groundTruth.append(x['bbox'])
+            #groundTruth.append(x['bbox'])
+            boundingBoxes[image_id]['annotations'].append(x['bbox'])
 
     with open('bboxes.json', 'w+') as f:
         json.dump(boundingBoxes, f, indent=4)
-    # TODO: Controlla gli indici, il for fallisce
-    """for box in range(0, len(bboxes)):
-        boxA = bboxes[box]['bbox']
-        for ann in annotations['annotations']:
-            if bboxes[box]['image_id'] == (ann['image_id'] - 150):
-                boxB = ann['bbox']
-                iou = utils.intersectionOverUnion(boxA, boxB)
-                if iou != 0.0:
-                    cv2.putText(img=image, text=str(round(iou, 2)),
-                                org=(int(boxB[0]), int(boxB[1]) + int(boxB[3] + 5)),
-                                fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(255, 0, 0))"""
 
-    boundingBoxes[image_id]['bboxes'].append(list(set(detections)))
-    boundingBoxes[image_id]['annotations'].append(list(set(groundTruth)))
-
+    # TODO: funziona, ma potrebbe funzionare molto meglio con un sistema per trovare
     for box in boundingBoxes[image_id]['bboxes']:
+        notes = []
         for ann in boundingBoxes[image_id]['annotations']:
-            iou = utils.intersectionOverUnion(box, ann)
-            if iou != 0.0:
-                if iou >= 0.5:
-                    cv2.putText(img=image, text='Y',
-                                org=(int(ann[0]), int(ann[1]) + int(ann[3] + 5)),
-                                fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(255, 0, 0))
-                    break
-                else:
-                    cv2.putText(img=image, text='N',
-                                org=(int(ann[0]), int(ann[1]) + int(ann[3] + 5)),
-                                fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(255, 0, 0))
-                    break
+            if (box[1] >= ann[1] - 20) and (box[1] <= ann[1] + 20):
+                iou = utils.intersectionOverUnion(box, ann)
+                if iou != 0.0 and ann not in notes:
+                    print(ann, image_id)
+                    if iou >= 0.5:
+                        cv2.putText(img=image, text='Y',
+                                    org=(int(box[0]), int(box[1]) + int(box[3] + 5)),
+                                    fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(255, 0, 0))
+                        notes.append(ann)
+                        break
+                    else:
+                        cv2.putText(img=image, text='N',
+                                    org=(int(box[0]), int(box[1]) + int(box[3] + 5)),
+                                    fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=0.4, color=(255, 0, 0))
+                        notes.append(ann)
+                        break
+
 
     """cv2.imshow('{image}'.format(image=img['file_name']), image)
     cv2.waitKey(100000)
